cbd838fe573f1e300eca320ac2b57eb47a9278a7
Pedro Manse <pedro.manse@dmk3.com.br>
2025-02-20 18:48:27 -0300

    failed on idem transforms and changed Tranform<A, To=B> to Transform<A, B>

--- src/state.rs
+++ src/state.rs
@@ -1,9 +1,35 @@
-trait Transform<ThisMario>
+pub trait ResultTranform<AMario, BMario>
+where
+    Self: Transformer,
+    AMario: Transformee,
+    BMario: Transformee,
+{
+}
+
+pub trait ManualTransform<ThisMario, ThatMario>
 where
     Self: Transformer,
     ThisMario: Transformee,
+    ThatMario: Transformee,
 {
-    type To: Default;
+}
+
+pub trait IdemTransform<ThisMario, ThatMario>
+where
+    Self: Transformer,
+    ThisMario: Transformee,
+    ThatMario: Transformee,
+{
+    type FromMario: Transformee;
+}
+
+pub trait SkipTransform<ThisMario, ThatMario>
+where
+    Self: Transformer,
+    ThisMario: Transformee,
+    ThatMario: Transformee,
+{
+    type Skip: Transformee;
 }
 
 trait Transformee {}
@@ -13,21 +39,21 @@ trait Idem {}
 macro_rules! make {
     (tranformee $name:ident) => {
         #[derive(Default)]
-        struct $name;
+        pub struct $name;
         impl Transformee for $name {}
     };
     (tranformer $name:ident) => {
+        pub struct $name;
         impl Transformer for $name {}
-        struct $name;
-    }
+    };
 }
 
 macro_rules! rule {
     ($tranformee:ident + $tranformer:ident => $transformed:ident) => {
-        impl Transform<$tranformee> for $tranformer { type To = $transformed; }
+        impl ManualTransform<$tranformee, $transformed> for $tranformer {}
     };
     ($tranformee:ident + $tranformer:ident => $transformed:ident idem) => {
-        impl Transform<$tranformee> for $tranformer { type To = $transformed; }
+        impl ManualTransform<$tranformee, $transformed> for $tranformer {}
         impl Idem for $tranformer {}
     };
 }
@@ -49,16 +75,39 @@ rule!(CapeMario + Damage => NormalMario);
 rule!(NormalMario + Damage => SmallMario);
 rule!(SmallMario + Damage => DeadMario);
 
-// for every idem-ponent transformation of a -t> b, create an idem-potent transformation of b -t> b
-///impl<T: Transformer, Mf: Transformee, Mr: Transformee> Transform<Mr> for T
-///where T: Transform<Mf> + Idem
-///{
-///    type To = Mf;
-///}
-
-// for every idem-potent tranformer that acts on NormalMario, replicate it's effect on SmallMario
-impl<T: Transformer> Transform<SmallMario> for T
-where T: Transform<NormalMario> + Idem
+// for every idem-ponent transformation of A -> B with T,
+// an idem-potent transformation of B -> B with T is crated
+impl<Flower, AMario, BMario> IdemTransform<BMario, BMario> for Flower
+where
+    Flower: ManualTransform<AMario, BMario> + Idem,
+    AMario: Transformee,
+    BMario: Transformee,
 {
-    type To = <T as Transform<NormalMario>>::To;
+    type FromMario = AMario;
+}
+
+// for every idem-potent tranformer that acts on NormalMario,
+// that same tranformer can act on SmallMario and generate the same Transformee
+impl<Flower, SuperMario: Transformee> SkipTransform<SmallMario, SuperMario> for Flower
+where
+    Flower: ManualTransform<NormalMario, SuperMario> + Idem,
+{
+    type Skip = NormalMario;
+}
+
+impl<AMario, BMario, Flower> ResultTranform<AMario, BMario> for Flower
+where
+    Flower: SkipTransform<AMario, BMario>,
+    AMario: Transformee,
+    BMario: Transformee,
+{
+}
+
+impl<Flower, AMario, BMario> ResultTranform<AMario, BMario> for Flower
+where
+    Flower: IdemTransform<AMario, BMario>,
+    AMario: Transformee,
+    BMario: Transformee
+
+{
 }
